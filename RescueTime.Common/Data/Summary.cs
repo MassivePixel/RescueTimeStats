fd445facc557ee643c8d7628adce2f178fae7e6e
Toni Petrina <info@tonicodes.net>
2014-03-04 22:06:02 +0100

    Calculating productivity

--- RescueTime.Common/Data/Summary.cs
+++ RescueTime.Common/Data/Summary.cs
@@ -9,16 +9,19 @@ namespace MassivePixel.RescueTime.Common.Data
         internal DefaultResponse OriginalResponse { get; set; }
         public List<RowInfo> Data { get; set; }
 
+        public Dictionary<int, int> Times { get; private set; }
+
+        public long Total { get; private set; }
+
+        public double Productivity { get; private set; }
+
         public void Calculate()
         {
             Times = Data.GroupBy(i => i.Productivity)
-                .ToDictionary(g => g.Key, g => TimeSpan.FromSeconds(g.Sum(i => i.TimeSpent)));
+                .ToDictionary(g => g.Key, g => g.Sum(i => i.TimeSpent));
 
-            Total = Times.Sum(i => i.Value.TotalSeconds);
+            Total = Times.Sum(i => i.Value);
+            Productivity = (double)Times.Where(i => i.Key > 0).Sum(i => i.Value) / Total * 100.0;
         }
-
-        public Dictionary<int, TimeSpan> Times { get; set; }
-
-        public double Total { get; set; }
     }
 }
