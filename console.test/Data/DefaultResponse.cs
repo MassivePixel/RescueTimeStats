4b06c78a0d650e017c2f5ffa0a83a2e23c01f258
Toni Petrina <info@tonicodes.net>
2014-03-04 20:35:48 +0100

    First API call

---
+++ console.test/Data/DefaultResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace console.test.Data
+{
+    class DefaultResponse
+    {
+        public string notes { get; set; }
+        public List<string> row_headers { get; set; }
+        public List<List<object>> rows { get; set; }
+    }
+}
--- console.test/Program.cs
+++ console.test/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace console.test
+﻿using System;
+using System.Net.Http;
+
+namespace console.test
 {
     class Program
     {
         static void Main()
         {
+            var client = new HttpClient();
+            var uri = string.Format("https://www.rescuetime.com/anapi/data?key={0}&format=json", ApiConfig.ApiKey);
+            try
+            {
+                var result = client.GetStringAsync(uri).Result;
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
